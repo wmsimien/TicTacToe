ad57786f9ff69d6bf348e0cb04883bc9c2e2d165
Wanda M Simien Avery <wanda.avery@att.net>
2023-03-30 20:14:00 -0500

    updated code base to handle setting the player's mark the same color as player's button

--- js/script.js
+++ js/script.js
@@ -1,37 +1,3 @@
-/**
-  Technical Requirements
-    Your Tic Tac Toe app must:
-
-        Render a game board in the browser
-        Switch turns between X and O (or whichever markers you select)
-        Visually display which side won if a player gets three in a row, or show a draw if neither player wins
-        Include separate HTML / CSS / JavaScript files
-        Stick with KISS (Keep It Simple Stupid) and DRY (Don't Repeat Yourself) principles
-        Use JavaScript for DOM manipulation
-        Deploy your game online, where the rest of the world can access it
-        You can use GitHub Pages for deploying your project
-        Use semantic markup for HTML and CSS (adhere to best practices)
-        Have well-formatted, and well-commented code
-User Stories
-        As a user, I should be able to start a new tic tac toe game
-        As a user, I should be able to click on a square to add X first and then O, and so on
-        As a user, I should be shown a message after each turn for if I win, lose, tie or who's turn it is next
-        As a user, I should not be able to click the same square twice
-        As a user, I should be shown a message when I win, lose or tie
-        As a user, I should not be able to continue playing once I win, lose, or tie
-        As a user, I should be able to play the game again without refreshing the page
-Potential Extra Tic Tac Toe Features
-        Keep track of multiple game rounds with a win, lose and tie counter
-        Allow players to customize their tokens (X, O, name, picture, etc)
-        Use localStorage to persist data locally to allow games to continue after page refresh or loss of 
-            internet connectivity
-        Involve Audio in your game
-        Create an AI opponent: teach JavaScript to play an unbeatable game against you
-        Make your site fully responsive so that it is playable from a mobile phone
-        Get inventive with your styling e.g. use hover effects or animations
-
- */
-
 // define and set variables
 const BOARD_LIMIT = 9;
 const player1BtnColor = '#FDB9FC';
@@ -182,6 +148,7 @@ const setPlayerMark = (player, squareLoc, plays) => {
     if (markedSquare.textContent.length === 0) {
         plays.push(Number(squareLoc));
         markedSquare.textContent = player === isPlayerX ? 'X' : 'O';
+        markedSquare.style.color = player === 'p1' ? player1BtnColor : player2BtnColor;
         playsCount++;  // increment the plays counter
     } else {
         alertMessage('Played', player);
